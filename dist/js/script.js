177a5d50e772c580003383b3f7912072054d16cb
ozix <ozix2990@gmail.com>
2021-06-10 22:30:00 +0300

    Promo

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,8 +3,12 @@
 ;
 $(`.header__mobile-menu`).on(`click`, function (e) {
     $(this).addClass(`header__mobile-menu_active`);
+    $(`.promo__scroll`).addClass(`promo__scroll_no-animate`);
     $(`.header__menu`).addClass(`header__menu_active`).on(`click`, function() {
         $(this).removeClass(`header__menu_active`);
       $(`.header__mobile-menu`).removeClass(`header__mobile-menu_active`);
-    })
+      setTimeout(() => {
+        $(`.promo__scroll`).removeClass(`promo__scroll_no-animate`);
+      }, 500);
+    });
 });
